e87a0420faa442d4e68c8b5e4f0ca45e31cced1f
Guest User <Guest@epicodus-15.local>
2018-04-10 14:24:53 -0700

    add css styling and fix image pathway

--- js/newscript.js
+++ js/newscript.js
@@ -53,9 +53,9 @@ function memoryFlipTile(tile,val){
 				    // Flip the 2 tiles back over
 				    var tile_1 = document.getElementById(memory_tile_ids[0]);
 				    var tile_2 = document.getElementById(memory_tile_ids[1]);
-				    tile_1.style.background = 'url(img/mario.jpg) no-repeat';
+				    tile_1.style.background = 'url(css/img/mario.jpg) no-repeat';
             	    tile_1.innerHTML = "";
-				    tile_2.style.background = 'url(img/mario.jpg) no-repeat';
+				    tile_2.style.background = 'url(css/img/mario.jpg) no-repeat';
             	    tile_2.innerHTML = "";
 				    // Clear both arrays
 				    memory_values = [];
