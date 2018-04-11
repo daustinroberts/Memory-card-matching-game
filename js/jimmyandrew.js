a9f966993cf38ff6bd6ea3f04953ead5a7b92a05
Jimmy McNamara <jmcnamara44@gmail.com>
2018-04-11 15:30:19 -0700

    add front end js

--- js/jimmyandrew.js
+++ js/jimmyandrew.js
@@ -1,11 +1,29 @@
 
-
+var initialArray = ["A","B","A","B"];
+var random = 1;
 
 
 $(document).ready(function() {
+
+  $("form#nameInput").submit(function(event) {
+    event.preventDefault();
+  });
+
+for(var i = 0; i<initialArray.length; i++) {
+  $("span#indexval" + (i+1)).text(initialArray[i]);
+
+};
+
+console.log(initialArray.length);
   $(".card").click(function() {
+    $(this).find("span").css("display", "block");
+
     // var cellValue = $(this).attr("p");
     var cardValue = ($(this).find("p").text());
+
+    // $(this).find("span").removeClass(".showCard");
+    // var showValue = ($(this).show());
     console.log(cardValue);
+
   });
 })
